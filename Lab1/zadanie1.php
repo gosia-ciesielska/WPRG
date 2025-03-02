c1f9d561e94eb6ac9f939fde5db3e504b1f0bd38
Malgorzata Ciesielska <s26974@pjwstk.edu.pl>
2025-03-02 13:43:49 +0100

    poprawki

--- Lab1/zadanie1.php
+++ Lab1/zadanie1.php
@@ -7,7 +7,8 @@
 
     for ($i = 0; $i < sizeof($fruit); $i++) {
         for ($j = strlen($fruit[$i]); $j >= 0; $j--) {
-            echo $fruit[$i][$j];
+            $word = $fruit[$i];
+            echo substr($word, $j, 1);
         }
         echo "; Starts with p: ";
         if ($fruit[$i][0] == 'p') {
