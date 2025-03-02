95630772f59efb23560b33a2401728b767f30269
Malgorzata Ciesielska <s26974@pjwstk.edu.pl>
2025-03-02 13:44:38 +0100

    Revert "poprawki"
    This reverts commit c1f9d561e94eb6ac9f939fde5db3e504b1f0bd38.

--- Lab1/zadanie1.php
+++ Lab1/zadanie1.php
@@ -7,8 +7,7 @@
 
     for ($i = 0; $i < sizeof($fruit); $i++) {
         for ($j = strlen($fruit[$i]); $j >= 0; $j--) {
-            $word = $fruit[$i];
-            echo substr($word, $j, 1);
+            echo $fruit[$i][$j];
         }
         echo "; Starts with p: ";
         if ($fruit[$i][0] == 'p') {
