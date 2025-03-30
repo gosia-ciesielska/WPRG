3473a6266327d819d243024ca689c4cb2ffaf251
Malgorzata Ciesielska <s26974@pjwstk.edu.pl>
2025-03-30 11:39:52 +0200

    Revert "zadania lab 2 pierwsza wersja"
    This reverts commit 9d98d84bf6aee2de6b48528460175fb8343e2f4f.

--- Lab2/zad2i3/summary.php
+++
@@ -1,7 +0,0 @@
-Przykład danych z pierwszego formularza: <?php echo $data->email; ?> <br>
-Przykład imion gości: <br>
-<?php
-for ($i = 0; $i < count($guests); $i++) {
-    echo $guests[$i]->name . '<br>';
-}
-?>
